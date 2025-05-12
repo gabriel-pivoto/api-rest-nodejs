5c580ff7931641ed89bf759b2adf7d7b69d338fd
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-05-12 10:21:54 -0300

    feat: migrations

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,14 @@
 // knex.ts
 import { Knex, knex as setupKnex } from 'knex'
 
-const config = {
+export const config: Knex.Config = {
   client: "sqlite",
   connection: {
     filename: "./db/app.db",
   },
   migrations: {
     extension: 'ts',
-    directory: './db/migrations',
+    directory: './migrations/',
   },
   useNullAsDefault: true,
 };

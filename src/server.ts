d013670498998c854b67f709f396bfb472d48545
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-05-12 10:27:10 -0300

    feat: queries db

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 // server.ts
 import fastify from "fastify";
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const test = await knex("sqlite_schema").select("*");
-  return test;
+  const transactions = await knex("transactions").insert({
+    id: crypto.randomUUID(),
+    title: "Test",
+    amount: 1000,
+    created_at: new Date(),
+  }).returning('*')
+
+  return transactions;
 });
 
 app.listen({ port: 3333 }).then(() => {

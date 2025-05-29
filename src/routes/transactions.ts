19566194aca348fbda7b1c2df92e2b4af4f159d0
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-05-29 19:46:56 -0300

    feat handler

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,9 @@ import { knex } from "../database";
 import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request, reply) => {
+    console.log(`Request received: ${request.method} ${request.url}`);
+  })
   app.get(
     "/",
     {

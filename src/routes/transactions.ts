efe7a91908050225885e70d583c4c4b7143a9aa8
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-05-29 10:29:47 -0300

    feat: transactions

---
+++ src/routes/transactions.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+import { z } from "zod";
+import { request } from "http";
+import crypto from "node:crypto";
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select("*");
+    return { transactions };
+  });
+  app.get("/:id", async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knex("transactions").where("id", id).first();
+
+    return { transaction };
+  });
+
+  app.get("/summary", async (request, reply) => {
+    const summary = await knex("transactions")
+      .sum("amount", { as: "amount" })
+      .first();
+    return { summary };
+  });
+  app.post("/", async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    );
+
+    const transaction = await knex("transactions").insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+      created_at: new Date(),
+    });
+
+    return reply.status(201).send();
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,14 @@
 import fastify from "fastify";
 import { knex } from './database'
 import crypto from 'node:crypto'
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  const transactions = await knex("transactions").insert({
-    id: crypto.randomUUID(),
-    title: "Test",
-    amount: 1000,
-    created_at: new Date(),
-  }).returning('*')
-
-  return transactions;
-});
+app.register(transactionsRoutes, {
+  prefix: "transactions",
+})
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running on http://localhost:3333");
+  console.log("HTTP server running on http://localhost:3333/hello");
 });
